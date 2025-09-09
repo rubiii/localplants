33cb012e3380ddcad1d9acd01de762cc3cd7449c
rubiii <me@rubiii.com>
2025-09-09 22:00:39 +0200

    Basic pluralization

--- src/lib/timeDifference.ts
+++ src/lib/timeDifference.ts
@@ -1,23 +1,33 @@
-export default function timeDifference(current: Date, previous: Date) {
-  const msPerMinute = 60 * 1000
-  const msPerHour = msPerMinute * 60
-  const msPerDay = msPerHour * 24
-  const msPerMonth = msPerDay * 30
-  const msPerYear = msPerDay * 365
+const msPerMinute = 60 * 1000
+const msPerHour = msPerMinute * 60
+const msPerDay = msPerHour * 24
+const msPerMonth = msPerDay * 30
+const msPerYear = msPerDay * 365
 
+export default function timeDifference(current: Date, previous: Date) {
   const elapsed = current.valueOf() - previous.valueOf()
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + " seconds ago"
+    const seconds = Math.round(elapsed / 1000)
+    return `${seconds} ${pluralize(seconds, "second")} ago`
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + " minutes ago"
+    const minutes = Math.round(elapsed / msPerMinute)
+    return `${minutes} ${pluralize(minutes, "minute")} ago`
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + " hours ago"
+    const hours = Math.round(elapsed / msPerHour)
+    return `${hours} ${pluralize(hours, "hour")} ago`
   } else if (elapsed < msPerMonth) {
-    return "~" + Math.round(elapsed / msPerDay) + " days ago"
+    const days = Math.round(elapsed / msPerDay)
+    return `${days} ${pluralize(days, "day")} ago`
   } else if (elapsed < msPerYear) {
-    return "~" + Math.round(elapsed / msPerMonth) + " months ago"
+    const months = Math.round(elapsed / msPerMonth)
+    return `${months} ${pluralize(months, "month")} ago`
   } else {
-    return "~" + Math.round(elapsed / msPerYear) + " years ago"
+    const years = Math.round(elapsed / msPerYear)
+    return `${years} ${pluralize(years, "year")} ago`
   }
+}
+
+function pluralize(count: number, word: string) {
+  return count === 1 ? word : `${word}s`
 }
