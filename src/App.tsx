a8a7ca20c1df55db9749f182f176e83bdb2b0c2d
rubiii <me@rubiii.com>
2025-09-15 17:50:52 +0200

    Add splashscreen and icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import Navigation from "@/Navigation"
 import { MyAppAccount } from "@/schema"
 import "@bam.tech/react-native-image-resizer"
 import AsyncStorage from "@react-native-async-storage/async-storage"
+import * as SplashScreen from "expo-splash-screen"
 import { JazzExpoProvider } from "jazz-tools/expo"
 import { StrictMode, useEffect, useState } from "react"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { KeyboardProvider } from "react-native-keyboard-controller"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+
+SplashScreen.preventAutoHideAsync()
+SplashScreen.setOptions({
+  duration: 3000,
+  fade: true,
+})
 
 const peer = `wss://cloud.jazz.tools/?key=me@rubiii.com`
 
@@ -16,9 +23,10 @@ export default function App() {
   const [skipWelcome, setSkipWelcome] = useState<boolean>()
 
   useEffect(() => {
-    AsyncStorage.getItem("skip-welcome").then((value) =>
-      setSkipWelcome(value === "true"),
-    )
+    AsyncStorage.getItem("skip-welcome").then((value) => {
+      setSkipWelcome(value === "true")
+      SplashScreen.hideAsync()
+    })
   }, [])
 
   if (skipWelcome === undefined) return
